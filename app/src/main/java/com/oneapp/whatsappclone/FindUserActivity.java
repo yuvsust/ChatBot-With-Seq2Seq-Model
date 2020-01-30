a762e3e9ed988f589b29a85c5df404b62bd7a6d3
yuvsust <>
2020-01-30 18:25:45 +0600

    User List View added

--- app/src/main/java/com/oneapp/whatsappclone/FindUserActivity.java
+++ app/src/main/java/com/oneapp/whatsappclone/FindUserActivity.java
@@ -7,20 +7,29 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+
 public class FindUserActivity extends AppCompatActivity {
 
     private RecyclerView mUserList;
     private RecyclerView.Adapter mUserListAdapter;
     private RecyclerView.LayoutManager mUserListLayoutManager;
+    ArrayList<UserObject> userList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
+
+        userList = new ArrayList<UserObject>();
 
         mUserList  = findViewById(R.id.userList);
         mUserList.setNestedScrollingEnabled(false);
         mUserList.setHasFixedSize(false);
         mUserListLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayout.VERTICAL, false);
         mUserList.setLayoutManager(mUserListLayoutManager);
+        mUserListAdapter = new UserListAdapter(userList);
+        mUserList.setAdapter(mUserListAdapter);
+
     }
 }
---
+++ app/src/main/java/com/oneapp/whatsappclone/UserObject.java
@@ -0,0 +1,19 @@
+package com.oneapp.whatsappclone;
+
+public class UserObject {
+    private String name, phone;
+
+    public UserObject(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+}
